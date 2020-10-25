3e395aa7a3e9e2838907d67494e80127674a814b
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2020-10-26 00:19:41 +0530

    add: description text

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,9 @@ let strings = {
     github_url: "https://github.com/sm3saurabh/",
     linkedin_url: "https://www.linkedin.com/in/saurabh-mishra-156693146/",
     medium_url: "https://medium.com/@sm3.saurabhmishra",
-    mm_url: "https://mutualmobile.com/"
+    mm_url: "https://mutualmobile.com/",
+    skia_url: "https://skia.org/",
+    android_url: "https://www.android.com/intl/en_in/"
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -14,28 +16,43 @@ document.addEventListener("DOMContentLoaded", function() {
 function setupSocialClick() {    
     document.getElementById("twitter").addEventListener("click", function(event) {
         event.preventDefault()
-        window.open(strings.twitter_url, "_blank")
+        openUrl(strings.twitter_url)
     })
 
     document.getElementById("github").addEventListener("click", function(event) {
         event.preventDefault()
-        window.open(strings.github_url, "_blank")
+        openUrl(strings.github_url)
     })
 
     document.getElementById("medium").addEventListener("click", function(event) {
         event.preventDefault()
-        window.open(strings.medium_url, "_blank")
+        openUrl(strings.medium_url)
     })
 
     document.getElementById("linkedin").addEventListener("click", function(event) {
         event.preventDefault()
-        window.open(strings.linkedin_url, "_blank")
+        openUrl(strings.linkedin_url)
     })
 }
 
 function setupLinkClick() {
+
     document.getElementById("mm").addEventListener("click", function(event) {
         event.preventDefault()
-        window.open(strings.mm_url, "_blank")
+        openUrl(strings.mm_url)
     })
+
+    document.getElementById("skia").addEventListener("click", function(event) {
+        event.preventDefault()
+        openUrl(strings.skia_url)
+    })
+
+    document.getElementById("android").addEventListener("click", function(event) {
+        event.preventDefault()
+        openUrl(strings.android_url)
+    })
+}
+
+function openUrl(url) {
+    window.open(url, "_blank")
 }
