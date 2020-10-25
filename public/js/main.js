9decea8f57c4149e43e8205d0a7ab53e16abe332
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2020-10-25 16:47:30 +0530

    add: links for social accounts. fix: again try out a background gradient

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,39 @@
+let strings = {
+    self_description: "I am an android developer I am an android developer I am an android developer I am an android developer I am an android developer I am an android developer ",
+    twitter_url: "https://twitter.com/sm_saurabhishra",
+    github_url: "https://github.com/sm3saurabh/",
+    linkedin_url: "https://www.linkedin.com/in/saurabh-mishra-156693146/",
+    medium_url: "https://medium.com/@sm3.saurabhmishra"
+}
 
+document.addEventListener("DOMContentLoaded", function() {
+    populateDescription()
+    setupSocialClick()
+})
 
+function populateDescription() {
+    let element = document.getElementById("description")
+    element.innerHTML = strings.self_description
+}
 
+function setupSocialClick() {    
+    document.getElementById("twitter").addEventListener("click", function(event) {
+        event.preventDefault()
+        window.open(strings.twitter_url, "_blank")
+    })
 
+    document.getElementById("github").addEventListener("click", function(event) {
+        event.preventDefault()
+        window.open(strings.github_url, "_blank")
+    })
+
+    document.getElementById("medium").addEventListener("click", function(event) {
+        event.preventDefault()
+        window.open(strings.medium_url, "_blank")
+    })
+
+    document.getElementById("linkedin").addEventListener("click", function(event) {
+        event.preventDefault()
+        window.open(strings.linkedin_url, "_blank")
+    })
+}
